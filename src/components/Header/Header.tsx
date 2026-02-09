76585d424f62af1c3d3ffb12aef6683b09b969f2
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-02-09 20:23:03 -0300

    refactor: replace div with nav in header for semantic HTML

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 export const Header = () => {
     return (
         <>
-            <div className="text-[#E5E7EB] flex justify-between p-10">
-                <a className="hover:text-[#A78BFA] transition-all" href="/">André Arcanjo</a>
-                <ul className="flex gap-5">
-                    <li><a className="hover:text-[#A78BFA] transition-all" href="#sobre">Sobre</a></li>
-                    <li><a className="hover:text-[#A78BFA] transition-all" href="#projetos">Projetos</a></li>
-                    <li><a className="hover:text-[#A78BFA] transition-all" href="#contato">Contato</a></li>
-                </ul>
-            </div>
+            <header>
+                <nav className="text-[#E5E7EB] flex justify-between p-10">
+                    <a className="hover:text-[#A78BFA] transition-all" href="/">André Arcanjo</a>
+                    <ul className="flex gap-5">
+                        <li><a className="hover:text-[#A78BFA] transition-all" href="#sobre">Sobre</a></li>
+                        <li><a className="hover:text-[#A78BFA] transition-all" href="#projetos">Projetos</a></li>
+                        <li><a className="hover:text-[#A78BFA] transition-all" href="#contato">Contato</a></li>
+                    </ul>
+                </nav>
+            </header>
         </>
     )
 }
