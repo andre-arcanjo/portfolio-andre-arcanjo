8940ed4050e7d6e805edf9087620f279ff735d5f
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-08-19 21:18:07 -0300

    feat: update contact section for full-stack profile

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,12 +9,14 @@ export const Contact = () => {
       <h2 className="font-bold text-3xl md:text-4xl">Contato</h2>
       <div className="text-center text-sm md:text-base max-w-90 md:max-w-120 space-y-2">
         <p>
-          Estou em busca da minha primeira oportunidade como desenvolvedor
-          Front-End.
+          <p>
+            Estou em busca da minha primeira oportunidade como desenvolvedor
+            Full Stack.
+          </p>
         </p>
         <p>
-          Se quiser conversar sobre vagas ou projetos, será um prazer falar com
-          você!
+          Se quiser conversar sobre oportunidades, projetos ou tecnologia, será
+          um prazer falar com você!
         </p>
       </div>
       <div className="flex flex-wrap justify-center gap-5">
