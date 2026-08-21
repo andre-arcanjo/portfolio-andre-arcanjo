e52ef29ca1c5d15f7fcbaa0339752558e368d4fd
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-08-20 21:25:46 -0300

    style: improve header spacing and responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { HeaderNav } from './HeadersNavs';
 export const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-[#0B0F19] z-50">
-      <nav className="lg:text-[20px] flex flex-col justify-between items-center px-30 py-10 md:flex-row">
+      <nav className="lg:text-[18px] flex flex-col justify-between items-center px-6 md:px-12 lg:px-30 py-5 md:flex-row">
         <a className="hover:text-hover-button transition-all" href="#hero">
           André Arcanjo
         </a>
